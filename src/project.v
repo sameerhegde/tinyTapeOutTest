90ac4f62dc7f946f565e5a6c5015a4a992782e3d
Sameer Hegde <109801628+sameerhegde@users.noreply.github.com>
2025-03-03 16:17:46 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -31,6 +31,6 @@ module tt_um_sameerhegde_adder (
   assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena,1'b0};
+	wire _unused = &{ena,uio_in,1'b0};
 
 endmodule
